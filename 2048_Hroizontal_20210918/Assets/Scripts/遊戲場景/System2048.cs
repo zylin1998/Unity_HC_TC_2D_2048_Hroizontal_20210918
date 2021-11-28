8924b206818bdd5e5c56b68c9ec1cbbb5e62352c
zylin1998 <72535355+zylin1998@users.noreply.github.com>
2021-11-28 16:27:17 +0800

    2021.11.28

--- 2048_Hroizontal_20210918/Assets/Scripts/遊戲場景/System2048.cs
+++ 2048_Hroizontal_20210918/Assets/Scripts/遊戲場景/System2048.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Linq;
 
 public enum Direction{
@@ -13,6 +14,8 @@ public class BlockData
     public Vector2 Position;
     public Vector2Int Index;
     public int number;
+
+    public BlockData() { }
 }
 
 public class System2048 : MonoBehaviour
@@ -26,11 +29,21 @@ public class System2048 : MonoBehaviour
     [Header("起始格數")]
     public int _initialBlocksNumber = 2;
 
-    public BlockData[,] _blockData = new BlockData[4,4];
+    [SerializeField]
+    private Direction direction;
+    private BlockData[,] _blockData = new BlockData[1,4];
+    private Vector3 _posDown;
+    private Vector3 _posUp;
+    private bool _isClick = false;
 
     private void Start() 
     { 
         Initialize();
+    }
+
+    private void Update()
+    {
+        CheckDirection();
     }
 
     private void Initialize()
@@ -58,7 +71,8 @@ public class System2048 : MonoBehaviour
         {
             for (int j = 0; j < _blockData.GetLength(1); j++)
             {
-                result += "ID<color=grey>" + _blockData[i, j].Index + "</color> Num[<color=grey>" + _blockData[i, j].number + "</color>] Posi<color=grey>" + _blockData[i,j].Position + "</color> | ";
+                
+                result += (_blockData[i, j].number > 0 ? _blockData[i, j].number : 0) +  " | ";
             }
 
             result += "\n";
@@ -82,5 +96,132 @@ public class System2048 : MonoBehaviour
         RandomBlock.number = 2;
 
         RandomBlock.goBlock = Instantiate(_numberBlocks,RandomBlock.Position, Quaternion.identity, _canvas2048);
+    }
+
+    private void CheckDirection() 
+    {
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            direction = Direction.Up;
+            CheckMoveDirection();
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Direction.Down;
+            CheckMoveDirection();
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Direction.Left;
+            CheckMoveDirection();
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Direction.Right;
+            CheckMoveDirection();
+        }
+
+        if(!_isClick && Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            _isClick = true;
+            _posDown = Input.mousePosition;
+        }
+        else if (_isClick && Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _isClick = false;
+            _posUp = Input.mousePosition;
+
+            Vector3 directionValue = (_posUp - _posDown).normalized;
+
+            float _xabs = Mathf.Abs(directionValue.x);
+            float _yabs = Mathf.Abs(directionValue.y);
+    
+            if(_xabs > _yabs) 
+            {
+                if (directionValue.x > 0) { direction = Direction.Right; }
+                else if (directionValue.x < 0) { direction = Direction.Left; }
+
+                CheckMoveDirection();
+            }
+
+            else if(_yabs > _xabs) 
+            {
+                if (directionValue.y > 0) { direction = Direction.Up; }
+                else if (directionValue.y < 0) { direction = Direction.Down; }
+
+                CheckMoveDirection();
+            }
+        }
+    }
+
+    public void CheckMoveDirection() 
+    {
+        Debug.Log("Now Direction: " + direction);
+
+        //int[,] locate;
+
+        switch (direction)
+        {
+            case Direction.Right:
+                break;
+            case Direction.Left:
+                for(int i = 0; i < _blockData.GetLength(0); i++) 
+                {
+                    for(int j = 0; j < _blockData.GetLength(1); j++) 
+                    {
+                        BlockData blockOrigin = new BlockData();
+                        BlockData blockCheck = new BlockData();
+                        bool canMove = false;
+                        bool sameNumber = false;
+                        blockOrigin = _blockData[i, j];
+
+                        if(blockOrigin.number == 0) { continue; }
+
+                        for(int k = j - 1; k >= 0; k--) 
+                        {
+                            if (_blockData[i, k].number == 0)
+                            {
+                                canMove = true;
+                                blockCheck = _blockData[i, k];
+                            }
+                            else if (_blockData[i, k].number == blockOrigin.number) 
+                            {
+                                blockCheck = _blockData[i, k];
+                                canMove = true;
+                                sameNumber = true;
+                            }
+                        }
+                        if (canMove) 
+                        { 
+                            blockOrigin.goBlock.transform.position = blockCheck.Position;
+                            if (sameNumber)
+                            {
+                                int number = blockCheck.number * 2;
+                                blockCheck.number = number;
+
+                                Debug.Log(blockCheck.number);
+
+                                Destroy(blockOrigin.goBlock);
+                                blockCheck.goBlock.transform.Find("數字").GetComponent<Text>().text = blockCheck.number.ToString();
+                            }
+                            else 
+                            { 
+                                blockCheck.number = blockOrigin.number;
+                                blockCheck.goBlock = blockOrigin.goBlock;
+                            }
+                            blockOrigin.number = 0;
+                            blockOrigin.goBlock = null;
+                        }                        
+                    }
+                }
+
+                DisplayBlockData();
+
+                break;
+            case Direction.Up:
+                break;
+            case Direction.Down:
+                break;
+        }
     }
 }
